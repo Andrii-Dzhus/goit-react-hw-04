25bb1505c3444bdd336515b50bfd863bc5fdb6af
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-08-30 15:32:51 +0300

    30/08/24

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,70 @@
 import { useState, useEffect } from "react";
-import contactDate from "../../contact.json";
-import ContactList from "../ContactList/ContactList";
-import SearchBox from "../SearchBox/SearchBox";
-import ContactForm from "../ContactForm/ContactForm";
+import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "../SearchBar/SearchBar";
+import { fetchPictures } from "../../fetchPictures";
+import ImageGallery from "../ImageGallery/ImageGallery";
+import { Toaster } from "react-hot-toast";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import ImageModal from "../ImageModal/ImageModal";
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const saveContacts = window.localStorage.getItem("contacts");
-    return saveContacts !== null ? JSON.parse(saveContacts) : contactDate;
-  });
+  const [topic, setTopic] = useState("");
+  const [page, setPage] = useState(1);
+  const [pictures, setPictures] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
-    window.localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
+    if (topic === "") {
+      return;
+    }
+    async function getPictures() {
+      try {
+        setLoading(true);
+        setError(false);
+        const newPictures = await fetchPictures(topic, page);
+        setPictures(prevState => [...prevState, ...newPictures]);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getPictures();
+  }, [topic, page]);
 
-  const [filter, setFilter] = useState("");
-
-  const addContacts = newContacts => {
-    setContacts(prevContacts => {
-      return [...prevContacts, newContacts];
-    });
+  const handleSearch = newTopic => {
+    setTopic(newTopic);
+    setPage(1);
+    setPictures([]);
   };
 
-  const deleteContacts = contactId => {
-    setContacts(prevContacts => {
-      return prevContacts.filter(contact => contact.id !== contactId);
-    });
+  const handleLoadMore = () => {
+    setPage(page + 1);
   };
 
-  const visibleContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const handleImageClick = imageSrc => {
+    setSelectedImage(imageSrc);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
   return (
     <div>
-      <h1>Phonebook</h1>
-      <ContactForm onAdd={addContacts} />
-      <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList contacts={visibleContacts} onDelete={deleteContacts} />
+      <SearchBar onSearch={handleSearch} />
+      <ImageGallery items={pictures} onImageClick={handleImageClick} />
+      {pictures.length > 0 && <LoadMoreBtn onClick={handleLoadMore} />}
+      {error && <ErrorMessage />}
+      {loading && <Loader />}
+      <ImageModal
+        isOpen={!!selectedImage}
+        onClose={closeModal}
+        imageSrc={selectedImage}
+      />
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 }
---
+++ src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,67 @@
+import Modal from "react-modal";
+import { useEffect } from "react";
+
+export default function ImageModal({ isOpen, onClose, imageSrc }) {
+  useEffect(() => {
+    const handleEsc = event => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, [onClose]);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="Image Modal"
+      style={{
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.75)",
+        },
+        content: {
+          position: "relative",
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          transform: "translate(-50%, -50%)",
+          padding: 0,
+          border: "none",
+          background: "none",
+          maxWidth: "80%",
+          maxHeight: "80%",
+          overflow: "hidden",
+        },
+      }}
+      onClick={onClose}
+    >
+      <div
+        onClick={e => e.stopPropagation()}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <img
+          src={imageSrc}
+          alt=""
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+            objectFit: "contain",
+            borderRadius: "10px",
+          }}
+        />
+      </div>
+    </Modal>
+  );
+}
